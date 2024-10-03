fe9211b559405008732852731b7a91ee6d445024
MattiasOtter <mattias__o@live.se>
2024-10-03 02:27:15 +0200

    app working as it should

--- utils/imagePaths.ts
+++ utils/imagePaths.ts
@@ -6,7 +6,6 @@ export const imagePaths: { [key: string]: string[] } = {
     require("../assets/images/ahri/Ahri_3.jpg"),
     require("../assets/images/ahri/Ahri_4.jpg"),
     require("../assets/images/ahri/Ahri_5.jpg"),
-    // Add more images as needed
   ],
   garen: [
     require("../assets/images/garen/Garen_0.jpg"),
@@ -14,7 +13,6 @@ export const imagePaths: { [key: string]: string[] } = {
     require("../assets/images/garen/Garen_2.jpg"),
     require("../assets/images/garen/Garen_3.jpg"),
     require("../assets/images/garen/Garen_4.jpg"),
-    // Add more images as needed
   ],
   lux: [
     require("../assets/images/lux/Lux_0.jpg"),
@@ -23,15 +21,13 @@ export const imagePaths: { [key: string]: string[] } = {
     require("../assets/images/lux/Lux_3.jpg"),
     require("../assets/images/lux/Lux_4.jpg"),
     require("../assets/images/lux/Lux_5.jpg"),
-    // Add more images as needed
   ],
   zed: [
     require("../assets/images/zed/Zed_0.jpg"),
     require("../assets/images/zed/Zed_1.jpg"),
     require("../assets/images/zed/Zed_2.jpg"),
     require("../assets/images/zed/Zed_3.jpg"),
-    require("../assets/images/zed/Zed_4.jpg"),
-    // Add more images as needed
+    require("../assets/images/zed/Zed_10.jpg"),
   ],
   jinx: [
     require("../assets/images/jinx/Jinx_0.jpg"),
@@ -39,7 +35,6 @@ export const imagePaths: { [key: string]: string[] } = {
     require("../assets/images/jinx/Jinx_2.jpg"),
     require("../assets/images/jinx/Jinx_3.jpg"),
     require("../assets/images/jinx/Jinx_4.jpg"),
-    // Add more images as needed
   ],
   thresh: [
     require("../assets/images/thresh/Thresh_0.jpg"),
@@ -47,7 +42,6 @@ export const imagePaths: { [key: string]: string[] } = {
     require("../assets/images/thresh/Thresh_2.jpg"),
     require("../assets/images/thresh/Thresh_3.jpg"),
     require("../assets/images/thresh/Thresh_4.jpg"),
-    // Add more images as needed
   ],
   katarina: [
     require("../assets/images/katarina/Katarina_0.jpg"),
@@ -55,26 +49,26 @@ export const imagePaths: { [key: string]: string[] } = {
     require("../assets/images/katarina/Katarina_2.jpg"),
     require("../assets/images/katarina/Katarina_3.jpg"),
     require("../assets/images/katarina/Katarina_4.jpg"),
-    // Add more images as needed
   ],
   leesin: [
-    require("../assets/images/lee-sin/LeeSin_0.jpg"),
-    require("../assets/images/lee-sin/LeeSin_1.jpg"),
-    require("../assets/images/lee-sin/LeeSin_2.jpg"),
-    require("../assets/images/lee-sin/LeeSin_3.jpg"),
-    require("../assets/images/lee-sin/LeeSin_4.jpg"),
-    // Add more images as needed
+    require("../assets/images/leesin/LeeSin_0.jpg"),
+    require("../assets/images/leesin/LeeSin_1.jpg"),
+    require("../assets/images/leesin/LeeSin_2.jpg"),
+    require("../assets/images/leesin/LeeSin_3.jpg"),
+    require("../assets/images/leesin/LeeSin_4.jpg"),
   ],
   riven: [
     require("../assets/images/riven/Riven_0.jpg"),
     require("../assets/images/riven/Riven_1.jpg"),
     require("../assets/images/riven/Riven_2.jpg"),
-    // Add more images as needed
+    require("../assets/images/riven/Riven_3.jpg"),
+    require("../assets/images/riven/Riven_4.jpg"),
   ],
   vayne: [
     require("../assets/images/vayne/Vayne_0.jpg"),
     require("../assets/images/vayne/Vayne_1.jpg"),
     require("../assets/images/vayne/Vayne_2.jpg"),
-    // Add more images as needed
+    require("../assets/images/vayne/Vayne_3.jpg"),
+    require("../assets/images/vayne/Vayne_4.jpg"),
   ],
 };
